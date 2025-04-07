1df66941f5ec7e866efa9afe7b59692e1da92ea5
Jacky Cheng <chjack568@gmail.com>
2025-04-07 15:37:44 -0700

    Install Google Maps API, add Google Maps to Location page.

--- project/web/app/location/page.tsx
+++ project/web/app/location/page.tsx
@@ -1,7 +1,33 @@
+"use client"
+
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./location.css";
+
+const containerStyle = {
+  width: '850px',
+  height: '850px'
+};
+
+const center = {
+  lat: 33.6846,
+  lng: -117.8265
+};
+
 export default function LocationPage() {
-    return (
-      <div>
-        Work-in-progress.
-      </div>
-    );
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  console.log("API Key:", apiKey);
+
+  return (
+    <div className="google-maps">
+      <LoadScript googleMapsApiKey={apiKey!}>
+        <GoogleMap 
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        />
+      </LoadScript>
+    </div>
+  );
 };
