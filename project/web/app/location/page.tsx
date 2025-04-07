d0357d731db11d883c461d996401da1d6dc16d82
Jacky Cheng <chjack568@gmail.com>
2025-04-07 16:39:45 -0700

    Add error-checking for LoadScript tag, add loader.

--- project/web/app/location/page.tsx
+++ project/web/app/location/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import Image from 'next/image';
+import loader from './loader.gif';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./location.css";
@@ -14,14 +16,18 @@ const center = {
 export default function LocationPage() {
   const [isLoaded, setIsLoaded] = useState(false);
 
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
   return (
     <div className="google-maps">
       <LoadScript 
-        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
+        googleMapsApiKey={apiKey!}
         onLoad={() => {
           console.log("Google Maps API Loaded");
+
           setIsLoaded(true);
         }}
+        onError={(e) => console.error("LoadScript Error:", e)}
       >
         {isLoaded ? (
           <GoogleMap 
@@ -30,7 +36,10 @@ export default function LocationPage() {
             zoom={10}
           />
         ) : (
-          <div>Loading map...</div>
+          <div className="loader">
+            <Image src={loader} alt="Loading" />
+            <h1>Fetching Data...</h1>
+          </div>
         )}
       </LoadScript>
     </div>
