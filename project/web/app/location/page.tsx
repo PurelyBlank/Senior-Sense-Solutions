7122fd277ff991bb236052e326cfd8c29fd77417
Jacky Cheng <chjack568@gmail.com>
2025-04-09 10:05:04 -0700

    Add map marker, InfoWindow component with lat/lng.

--- project/web/app/location/page.tsx
+++ project/web/app/location/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import Image from 'next/image';
 import loader from './loader.gif';
 
@@ -13,10 +13,29 @@ const center = {
   lng: -117.8265
 };
 
+// Track position of map marker
+// REPLACE WITH COORDINATES OF THE WATCH AFTER RETRIEVING FROM DATABASE
+const markerPosition = {
+  lat: 33.6846,
+  lng: -117.8265
+};
+
 export default function LocationPage() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [infoOpen, setInfoOpen] = useState(false);
 
+  // Use JavaScript Google Maps API key defined in .env file
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  if (!apiKey) {
+    throw new Error("Google Maps API key is not defined in environment variables.");
+  }
+
+  // Handle close InfoWindow when map is clicked
+  const handleMapClick = () => {
+    if (infoOpen) {
+      setInfoOpen(false);
+    }
+  };
 
   return (
     <div className="google-maps">
@@ -29,12 +48,39 @@ export default function LocationPage() {
         }}
         onError={(e) => console.error("LoadScript Error:", e)}
       >
+        {/* Display Google Maps if loading is complete, show loader icon while it is incomplete */}
         {isLoaded ? (
           <GoogleMap 
             mapContainerClassName="map-container"
             center={center}
             zoom={10}
-          />
+            onClick={handleMapClick}
+          >
+            {/* Red marker icon */}
+            <Marker
+              position={markerPosition}
+              onClick={(e) => {
+                e.domEvent.stopPropagation();
+
+                setInfoOpen(true)
+              }}
+            >
+              {infoOpen && (
+                <InfoWindow
+                  position={markerPosition}
+                  onCloseClick={() => setInfoOpen(false)}
+                >
+                  <div className="info-window">
+                    <h3>Bruce Wayne</h3>
+                    <div className="lat-lng">
+                      <p className="text-secondary">Latitude: {markerPosition.lat}</p>
+                      <p className="text-secondary">Longitude: {markerPosition.lng}</p>
+                    </div>
+                  </div>
+                </InfoWindow>
+              )}
+            </Marker>
+          </GoogleMap>
         ) : (
           <div className="loader">
             <Image src={loader} alt="Loading" />
