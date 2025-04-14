a0a1a4095289790702ae3c7b595d68a7b59f3f0d
Tyler Tran <tylert6@uci.edu>
2025-04-14 13:32:23 -0700

    refactored Notification chart into it's own component

--- project/web/app/biometric-monitor/page.tsx
+++ project/web/app/biometric-monitor/page.tsx
@@ -12,7 +12,8 @@ import Link from "next/link";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./layout.css";
-import HeartRate from './heartRate';
+import HeartOxygenCharts from './HeartOxygenCharts';
+import NotificationChart from './NotificationChart';
 
 export default function HomePage() {
   const [isAddPatient, setIsAddPatient] = useState(false);
@@ -320,47 +321,10 @@ export default function HomePage() {
         </div>
 
         <div className="right-container">
-          {/* Notifications Box */}
-          <div className="notifications-box">
-            <p className="fw-semibold">Notifications</p>
-            <p className="abnormality-text">Abnormality Alerts</p>
-
-            { /*Notification Rows*/ }
-            <div className="notifications-details">
-              <div className="notifications-row">
-                <FiBell size={20} />
-                <div className="notifications-text">
-                  <span className="notifications-title">Missed Pill</span>
-                  <span className="notifications-summary">Medicine A at 2pm</span>
-                </div>
-              </div>
-              <div className="notifications-row">
-                <IoPersonOutline size={20} />
-                <div className="notifications-text">
-                  <span className="notifications-title">Heart Rate</span>
-                  <span className="notifications-summary">Too high: 180/min at 12pm</span>
-                </div>
-              </div>
-              <div className="notifications-row">
-                <IoPersonOutline size={20} />
-                <div className="notifications-text">
-                  <span className="notifications-title">Accelerometer</span>
-                  <span className="notifications-summary">A drop 2 min ago, check in with the patient</span>
-                </div>
-              </div>
-              <div className="notifications-row">
-                <IoPersonOutline size={20} />
-                <div className="notifications-text">
-                  <span className="notifications-title">Activity</span>
-                  <span className="notifications-summary">Less movement than usual in restroom, check in with the patient</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <HeartRate/>
-
+          <NotificationChart/>
+          <HeartOxygenCharts/>
         </div>
+        
       </div>
       {isRemovePatient && (
         <div className="center-remove-box">
