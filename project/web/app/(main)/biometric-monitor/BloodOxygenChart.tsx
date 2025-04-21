f3562524784fa3ec61d673fd037300a081042e15
Noah Wang <bwang.noah@gmail.com>
2025-04-20 17:34:57 -0700

    modified biometric reading guages

--- project/web/app/(main)/biometric-monitor/BloodOxygenChart.tsx
+++ project/web/app/(main)/biometric-monitor/BloodOxygenChart.tsx
@@ -7,7 +7,13 @@ export default function BloodOxygenChart(){
             <MdOutlineBloodtype size={45} />
             <p className="bloodoxygen-title fw-semibold">Blood Oxygen</p>
             <p className="bloodoxygen-percentage fw-semibold">88%</p>
-            <p className="bloodoxygen-summary fw-semibold">↓ Lower than average</p>
+            <p className="bloodoxygen-summary fw-semibold">
+                <em>
+                *Note: These readings reflect the patient's current blood oxygen level only. 
+                Please consult the guidelines <a href="https://www.ncbi.nlm.nih.gov/books/NBK470348/" target="_blank">link/reference</a> or 
+                professional healthcare advisor for proper interpretation.
+                </em>
+            </p>
         </div>
 
     )
--- project/web/app/(main)/biometric-monitor/HeartRateChart.tsx
+++ project/web/app/(main)/biometric-monitor/HeartRateChart.tsx
@@ -7,7 +7,13 @@ export default function HeartRateChart(){
             <FaHeartbeat size={45} />
             <p className="heartrate-title fw-semibold">Heart Rate</p>
             <p className="heartrate-bpm fw-semibold">120 BPM</p>
-            <p className="heartrate-summary fw-semibold">↑ Higher than average</p>
+            <p className="heartrate-summary fw-semibold">
+              <em>
+                  *Note: These readings reflect the patient's current heart rate. 
+                  Please consult the guidelines <a href="https://www.ncbi.nlm.nih.gov/books/NBK593193/table/ch1survey.T.normal_heart_rate_by_age/" target="_blank">link/reference</a> or 
+                  professional healthcare advisor for proper interpretation.
+              </em>
+            </p>
         </div>
     )
 }
