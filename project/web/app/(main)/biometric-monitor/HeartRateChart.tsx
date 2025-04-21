131b77e320a7e492ee27601c5c2ec87230f82510
Elijah Hahm <p1kachuthunders88@gmail.com>
2025-04-21 13:55:21 -0700

    Going back to semibold

--- project/web/app/(main)/biometric-monitor/HeartRateChart.tsx
+++ project/web/app/(main)/biometric-monitor/HeartRateChart.tsx
@@ -59,9 +59,9 @@ export default function HeartRateChart(){
     return(
         <div className="heartrate-box">
             <FaHeartbeat size={45} />
-            <p className="heartrate-title">Heart Rate</p>
-            <p className="heartrate-bpm">{heartRate_patient || "..."} BPM</p>
-            <p className="heartrate-summary">
+            <p className="heartrate-title fw-semibold">Heart Rate</p>
+            <p className="heartrate-bpm fw-semibold">{heartRate_patient || "..."} BPM</p>
+            <p className="heartrate-summary fw-semibold">
               <em>
                   *Note: This heart rate reading shows the patient's pulse right now. To understand what a healthy heart rate range is, please refer to the guidelines
                   Please consult the guidelines <a href="https://www.ncbi.nlm.nih.gov/books/NBK593193/table/ch1survey.T.normal_heart_rate_by_age/" target="_blank">link/reference</a> for proper interpretation.
