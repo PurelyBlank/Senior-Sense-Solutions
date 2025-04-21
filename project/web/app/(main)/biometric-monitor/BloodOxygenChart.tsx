2afa5d3a46eee554ee97942b15630953c29a8315
Noah Wang <bwang.noah@gmail.com>
2025-04-20 18:22:56 -0700

    merge main to branch

--- project/web/app/(main)/biometric-monitor/BloodOxygenChart.tsx
+++ project/web/app/(main)/biometric-monitor/BloodOxygenChart.tsx
@@ -9,8 +9,8 @@ export default function BloodOxygenChart(){
             <p className="bloodoxygen-percentage fw-semibold">88%</p>
             <p className="bloodoxygen-summary fw-semibold">
                 <em>
-                *Note: This is the patient's blood oxygen reading at this moment. It doesn't tell us if their oxygen is generally high or low.
-                Please consult the guidelines <a href="https://www.ncbi.nlm.nih.gov/books/NBK470348/" target="_blank">link/reference</a> proper interpretation.
+                    *Note: This is the patient's blood oxygen reading at this moment. It doesn't tell us if their oxygen is generally high or low.
+                    Please consult the guidelines <a href="https://www.ncbi.nlm.nih.gov/books/NBK470348/" target="_blank">link/reference</a> proper interpretation.
                 </em>
             </p>
         </div>
