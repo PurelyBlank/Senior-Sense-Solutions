93ba2a65be61f9f6dc8d51d1f298840de40d9dc9
Noah Wang <bwang.noah@gmail.com>
2025-04-28 15:14:40 -0700

    heart rate 1 second interval

--- project/embedded/src/wearable/wearable.ino
+++ project/embedded/src/wearable/wearable.ino
@@ -2,17 +2,23 @@
 
 #include <Wire.h>
 
-
+unsigned long previousSecond = 0;
+constexpr int oneSecond = 1000;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Initializing...");
 
+  // initialize all modules and sensors
   initialize_heart_rate_sensor();
 }
 
 void loop() {
-  // Serial.println(heart_rate());
-  Serial.println(millis());
-  delay(5000);
+  heart_rate();
+
+  unsigned long currentSecond = millis();
+  if (currentSecond - previousSecond >= oneSecond) {
+    previousSecond = currentSecond;
+    Serial.println(heart_rate());
+  }
 }
