cb21f73c04d20cc7a12c77bd6774bd92606727a0
Tyler Tran <tylerttrann@gmail.com>
2025-05-10 21:34:08 -0700

    seperated the fall report into it's own component

--- project/web/app/(main)/predictive-analysis/fallChart.tsx
+++ project/web/app/(main)/predictive-analysis/fallChart.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Chart, registerables } from "chart.js";
 import styles from "./charts.module.css";
+import FallReport from "./FallReport";
 
 Chart.register(...registerables);
 
@@ -11,7 +12,9 @@ export default function FallChart() {
 
   const [activateFallChart, setactivateFallChart] = useState(false); 
   const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [detectFall, setDetectFall] = useState(false);  // set true when the watch detects a fall 
 
+  
   useEffect(() => {
     if (chartRef.current) {
       if (chartInstanceRef.current) {
@@ -82,7 +85,6 @@ export default function FallChart() {
 
         {/* Description */}
         <div className={styles.BarChartDescription}>
-          <h1>Trending down by 50% over the last 3 weeks</h1>
           <p>Showing total falls over the last six weeks</p>
         </div>
       </div>
@@ -92,60 +94,16 @@ export default function FallChart() {
         <div className="overlay">
           <div className="center-remove-box">
             {!selectedDate ? (
-              <>
-                <p className="title-bold">Past Falls From 1/20–1/26</p>
-                <p>Click on a date to see more details about the event</p>
-
-                <div className={styles.fallDetailsContainer}>
-                  <h1 className={styles.fallButton} onClick={() => setSelectedDate("January 22, 2025")}>
-                    January 22, 2025
-                  </h1>
-                  <h1 className={styles.fallButton} onClick={() => setSelectedDate("January 24, 2025")}>
-                    January 24, 2025
-                  </h1>
-                </div>
-
-                <button type="button" className="cancel-button" onClick={() => setactivateFallChart(false)}>
-                  Exit
-                </button>
-              </>
-            ) : (
-              <>
-                <h1 className="title-bold mb-5">Detailed Fall Report for {selectedDate}</h1>
-
-                  <div>
-                    <div className = {styles.detailedFallReport}>
-                        <div>
-                          <p>Date and Time: </p>
-                        </div>
-
-                        <div>
-                          <p> January 24, 2025 7:23 PM</p>
-                        </div>
-                    </div>
-
-
-                    <div className = {styles.detailedFallReport}>
-                        <div>
-                          <p>Location: </p>
-                        </div>
-
-                        <div>
-                          <p> Aldrich Park, Irvine CA 92617</p>
-                        </div>
-                    </div>
-                    
-                  </div>
-                <button type="button" className="cancel-button" onClick={() => setSelectedDate(null)}>
-                  Back
-                </button>
-              </>
-            )}
-
-
+              <FallReport
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+                setactivateFallChart={setactivateFallChart}
+              />
+            ) : null}
           </div>
         </div>
       )}
+
     </div>
   );
 }
