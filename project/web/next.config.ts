7ebd6d9a3f9c49467cf1368464a5bee47898a041
ElijahHahm <125610262+ElijahHahm@users.noreply.github.com>
2025-05-19 13:37:12 -0700

    Update next.config.ts

--- project/web/next.config.ts
+++ project/web/next.config.ts
@@ -4,10 +4,4 @@ const nextConfig: NextConfig = {
   reactStrictMode: true, 
 };
 
-module.exports = {
-  env: {
-    NEXT_PUBLIC_API_URL: 'http://localhost:5000/api',
-  },
-};
-
 export default nextConfig;
